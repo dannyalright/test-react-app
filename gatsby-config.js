7f7c3fa57f7f036f54f9e14d2e36922e33aeed4b
Danny White <white.daniel.james@gmail.com>
2018-07-26 09:43:16 -0400

    first manual commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'A Test Website',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This is a test</h1>
+    <p>Not lying about this.</p>
+    <Link to="/page-2/">This is a big change.</Link>
+  </div>
+)
+
+export default IndexPage
